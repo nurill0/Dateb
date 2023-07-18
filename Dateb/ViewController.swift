06b668ab5ac729a8981e0e856f88335e26c0d4e2
Nurillo <nurillod23@mail.ru>
2023-07-18 11:45:14 +0500

    Initial Commit

---
+++ Dateb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dateb
+//
+//  Created by Nurillo Domlajonov on 18/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
